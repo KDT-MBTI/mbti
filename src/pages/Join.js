ac22488ce1874040d6baf218ce27263163c48c7e
zzinis <lizzyin@naver.com>
2023-07-03 15:12:41 +0900

    main page 수정

---
+++ src/pages/Join.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Wrapper, Inner } from './Main';
+
+
+
+function Join() {
+    return (
+        <>
+            <Wrapper>
+                <Inner>
+                    <h1>회원가입</h1>
+                </Inner>
+
+            </Wrapper>
+        </>
+    )
+}
+
+export default Join
